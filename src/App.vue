4e39b098666a0b6dfe792994a4f15d6c24c6381b
josephxia <15906475@qq.com>
2022-08-29 09:26:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/smarty.svg" style="filter: invert(100%);" class="logo" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue + SmartyUI" />
+  <div style="margin-bottom:20px;">
+    <SButton color="blue">主要按钮</SButton>
+    <SButton color="green">绿色按钮</SButton>
+    <SButton color="gray">灰色按钮</SButton>
+    <SButton color="yellow">黄色按钮</SButton>
+    <SButton color="red">红色按钮</SButton>
+  </div>
+  <div style="margin-bottom:20px;">
+    <SButton color="blue" plain>朴素按钮</SButton>
+    <SButton color="green" plain>绿色按钮</SButton>
+    <SButton color="gray" plain>灰色按钮</SButton>
+    <SButton color="yellow" plain>黄色按钮</SButton>
+    <SButton color="red" plain>红色按钮</SButton>
+  </div>
+  <div style="margin-bottom:20px;">
+    <SButton size="small" plain>小按钮</SButton>
+    <SButton size="medium" plain>中按钮</SButton>
+    <SButton size="large" plain>大按钮</SButton>
+  </div>
+  <div style="margin-bottom:20px;">
+    <SButton color="blue" round plain icon="search">搜索按钮</SButton>
+    <SButton color="green" round plain icon="edit">编辑按钮</SButton>
+    <SButton color="gray" round plain icon="check">成功按钮</SButton>
+    <SButton color="yellow" round plain icon="message">提示按钮</SButton>
+    <SButton color="red" round plain icon="delete">删除按钮</SButton>
+  </div>
+  <div style="margin-bottom:20px;">
+    <SButton color="blue" round plain icon="search"></SButton>
+    <SButton color="green" round plain icon="edit"></SButton>
+    <SButton color="gray" round plain icon="check"></SButton>
+    <SButton color="yellow" round plain icon="message"></SButton>
+    <SButton color="red" round plain icon="delete"></SButton>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
