89a8bd4fd37e1659cb8dbf7b2ea6662921856b49
josephxia <15906475@qq.com>
2022-11-07 12:06:15 +0800

    feat: update

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import "smarty-ui-vite/style.css";
-import SmartyUI from "smarty-ui-vite";
-
-createApp(App).use(SmartyUI).mount("#app");
